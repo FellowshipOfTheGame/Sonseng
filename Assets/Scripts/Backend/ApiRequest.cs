85c7890205b8cdd698f3bb0dc4f62489b92652b4
danielbarretto <daniel_barretto98@hotmail.com>
2020-05-25 16:35:13 -0300

    Add auth routes, google sign in and firebase plugins

--- Assets/Scripts/Backend/ApiRequest.cs
+++ Assets/Scripts/Backend/ApiRequest.cs
@@ -1,61 +1,126 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Google;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 public class ApiRequest : MonoBehaviour {
 
-    public InputField playerText;
-    public static int playerScore;
-    public static string playerName;
+    public Text debugText;
+    private const string WEB_CLIENT_ID = "321646647310-693rmelrk5re3cm29f2jc969n6t2mnpd.apps.googleusercontent.com";
+    private bool _loginStarted;
+    private string _token;
+    private string _errorMsg, errorDebug;
+    Firebase.Auth.FirebaseAuth auth;
+    private GoogleSignInConfiguration configuration;
 
-    public Text scoreText, receivedText;
+    void Awake() {
+        configuration = new GoogleSignInConfiguration {
+            WebClientId = WEB_CLIENT_ID,
+            RequestIdToken = true
+        };
+    }
     private void Start() {
         System.Random random = new System.Random();
-        playerScore = random.Next(0, 101);
-        scoreText.text = "Score: " + playerScore;
+
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
     }
 
-    public void StartRequest() {
-        playerName = playerText.text;
-        //StartCoroutine(GetRequest());
-        StartCoroutine(PostRequest());
+
+
+    public void SignInGoogle() {
+        
+        try {
+
+            GoogleSignIn.Configuration = configuration;
+
+            Task<GoogleSignInUser> signIn = GoogleSignIn.DefaultInstance.SignIn();
+
+            TaskCompletionSource<FirebaseUser> signInCompleted = new TaskCompletionSource<FirebaseUser>();
+            signIn.ContinueWith(task => {
+                if (task.IsCanceled) {
+                    signInCompleted.SetCanceled();
+                    errorDebug="Cancelada\n";
+                    return;
+                }
+                if (task.IsFaulted) {
+                    signInCompleted.SetException(task.Exception);
+                    errorDebug="Falhou\n";
+                    return;
+                }
+                errorDebug += "Completou : " + task.Result.Email + "\n";
+                Firebase.Auth.Credential credential =
+                    Firebase.Auth.GoogleAuthProvider.GetCredential(task.Result.IdToken, task.Result.AuthCode);
+                auth.SignInWithCredentialAsync(credential).ContinueWith(authTask => {
+                    if (authTask.IsCanceled) {
+                        Debug.LogError("SignInWithCredentialAsync was canceled.");
+                        errorDebug += "SignInWithCredentialAsync was canceled.\n";
+                        return;
+                    } else if (authTask.IsFaulted) {
+                        Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                        errorDebug += "SignInWithCredentialAsync encountered an error: " + task.Exception;
+                        return;
+                    }
+
+                    Firebase.Auth.FirebaseUser user = authTask.Result;
+                    string name = user.DisplayName;
+                    string email = user.Email;
+                    string uid = user.UserId;
+                    errorDebug += $"Name{name}\nEmail:{email}\nId:{uid}\n";
+                    signInCompleted.SetResult(user);
+                });
+
+            });
+            errorDebug += signInCompleted.Task.Result.DisplayName;
+            debugText.text = errorDebug;
+        } catch (System.Exception err) {
+            debugText.text = err.ToString();
+        }
+        //StartCoroutine(SignIn());
+        //Application.OpenURL("https://localhost:5000/auth/google");
     }
 
-    public void GetScoreFromServer() {
-        StartCoroutine(GetScore());
+    private IEnumerator SignIn() {
+        UnityWebRequest request = WebServices.Get("auth/google/");
+        WebServices.CookieString = null;
+        yield return request.SendWebRequest();
+        if (request.isNetworkError) {
+            Debug.LogError("ERROR!\n" + request.error);
+        } else {
+            Debug.Log(request.downloadHandler.text);
+        }
     }
 
+    public void SignOut() {
+        auth.SignOut();
+    }
+
+    /*
     private IEnumerator GetScore() {
-        WWWForm form = new WWWForm();
-        form.AddField("userName", playerText.text);
-
-        UnityWebRequest uwr = UnityWebRequest.Post("https://34.95.247.47:5000/getScore", form);
-        CustomCertificateHandler certHandler = new CustomCertificateHandler();
-        uwr.certificateHandler = certHandler;
-        yield return uwr.SendWebRequest();
-        if (uwr.isNetworkError) {
-            Debug.Log("Error while sending: " + uwr.error);
-        } else {
-            receivedText.text = "Score Recebida: " + uwr.downloadHandler.text;
-        }
-    }
-
-    private IEnumerator PostRequest() {
         User user = new User();
-        WWWForm form = new WWWForm();
-        form.AddField("userScore", user.userScore);
-        form.AddField("userName", user.userName);
-
-        UnityWebRequest uwr = UnityWebRequest.Post("https://34.95.247.47:5000/test", form);
-        CustomCertificateHandler certHandler = new CustomCertificateHandler();
-        uwr.certificateHandler = certHandler;
-        yield return uwr.SendWebRequest();
-        if (uwr.isNetworkError) {
-            Debug.Log("Error while sending: " + uwr.error);
+        UnityWebRequest request = WebServices.Post("getScore", JsonUtility.ToJson(user));
+        WebServices.CookieString = null;
+        yield return request.SendWebRequest();
+        if (request.isNetworkError) {
+            Debug.LogError("ERROR!\n" + request.error);
         } else {
-            Debug.Log("Received: " + uwr.responseCode);
+            receivedText.text = "Score Recebida " + request.downloadHandler.text;
+        }
+    }
+
+    private IEnumerator RegisterScore() {
+        User user = new User();
+        UnityWebRequest request = WebServices.Post("test", JsonUtility.ToJson(user));
+        WebServices.CookieString = null;
+        yield return request.SendWebRequest();
+        if (request.isNetworkError) {
+            Debug.LogError("ERROR!\n" + request.error);
+        } else {
+            Debug.Log("Received: " + request.responseCode);
         }
 
-    }
+    }*/
 }
--- SonsengBackend/connect.sh
+++
@@ -1,1 +0,0 @@
-gcloud beta compute ssh --zone "southamerica-east1-a" "sonseng-backend" --project "sonseng2020-1586957105557"
