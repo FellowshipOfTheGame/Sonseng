459d954e87ffbc70a7787db13da4fb2a35800542
HenriqueCamillo <henriquecamillo@usp.br>
2020-08-07 13:05:17 -0300

    Main menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
