ac2449e9ad79a8b2ca2ed5a81c885591fa2c63a8
danbarretto <daniel_barretto98@hotmail.com>
2020-08-26 14:14:09 -0300

    Add Store and Additional buttons

---
+++ Assets/UI/UpgradeButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class UpgradeButton : MonoBehaviour {
+    public TextMeshProUGUI costTxt;
+
+    public string cost;
+
+    void Start() {
+        costTxt.text = cost;
+    }
+
+    public void HandleClick(){
+        Debug.Log("Ha");
+    }
+}
