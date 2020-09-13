821a51d0dfe15ecd8c43a890f50a78134bd50460
Daniel Fróes <danielfroes15@gmail.com>
2020-09-13 19:18:31 -0300

    efeito de explosão completo e já integrado no jogo

--- Assets/Scripts/Objects/Obstacle.cs
+++ Assets/Scripts/Objects/Obstacle.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
 public class Obstacle : DestructableObject {
+
+    [SerializeField] private GameObject explosionContainer;
     private void Start() {
         if(PowerUps.instance)
             PowerUps.instance.OnPSwtichActivated += TransformIntoCoin;
@@ -8,12 +10,14 @@ public class Obstacle : DestructableObject {
 
     public override void Destroy() {
         // TODO add any effects here
+        explosionContainer.gameObject.SetActive(true);
         this.transform.parent.gameObject.SetActive(false);
     }
 
     public override void TransformIntoCoin() {
         // TODO add any effects here
         // TODO use object pooling
+        explosionContainer.gameObject.SetActive(true);
         Instantiate(PowerUps.instance.coinPrefab, this.transform.position, Quaternion.identity);
         this.transform.parent.gameObject.SetActive(false);
     }
--- Assets/Scripts/Rendering/BendingManager.cs
+++ Assets/Scripts/Rendering/BendingManager.cs
@@ -11,11 +11,9 @@ public class BendingManager : MonoBehaviour
 
   private const string PLANET_FEATURE = "ENABLE_BENDING_PLANET";
 
-  private static readonly int BENDING_AMOUNT_X =
-    Shader.PropertyToID("_BendingAmountX");
+  private static readonly int BENDING_AMOUNT_X = Shader.PropertyToID("_BendingAmountX");
 
-  private static readonly int BENDING_AMOUNT_Y =
-    Shader.PropertyToID("_BendingAmountY");
+  private static readonly int BENDING_AMOUNT_Y = Shader.PropertyToID("_BendingAmountY");
 
   #endregion
 
@@ -58,6 +56,9 @@ public class BendingManager : MonoBehaviour
 
     UpdateBendingAmountX();
     UpdateBendingAmountY();
+
+
+    print(BENDING_AMOUNT_X);
   }
 
   private void OnEnable ()
