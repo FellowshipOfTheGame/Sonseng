13ec3bbff7aafac96f1d3ccaabfaf22ebc49f1ea
danbarretto <daniel_barretto98@hotmail.com>
2020-09-19 18:37:28 -0300

    Fix save stats

--- SonsengBackend/functions/controller/userController.js
+++ SonsengBackend/functions/controller/userController.js
@@ -8,22 +8,36 @@ router.use(verifyMiddleWare)
 router.post('/saveStats', async (req, res) => {
   const { uid, score, cogs } = req.body
   const user = await admin.database().ref(`/users/${uid}`).once('value')
-
-  let highestScore = user.child('highest-score').val()
+  const scoreNumber = Number.parseInt(score)
+  let highestScore = user.child('highest-score')
+  if(highestScore.exists()){
+    highestScore = Number.parseInt(highestScore.val())
+  }else{
+    highestScore = 0
+  }
   try {
-    if (score > highestScore) {
-      user.ref.update({ 'highest-score': score, 'last-score': score })
-      highestScore = score
-    } else user.ref.update({ 'last-score': score })
-    let coins = Number.parseInt(user.child('coins').val())
-    await user.ref.update({ coins: coins + Number.parseInt(cogs) })
-    coins += Number.parseInt(cogs)
-    return res.send({ highestScore, cogs: coins })
+    if (scoreNumber > highestScore) {
+      user.ref.update({
+        'highest-score': Number.parseInt(score),
+        'last-score': scoreNumber,
+      })
+      highestScore = scoreNumber
+    } else user.ref.update({ 'last-score': scoreNumber })
+    let coins = user.child('coins')
+    if (coins.exists()) {
+      coins = Number.parseInt(coins.val())
+      console.log(coins)
+      coins += Number.parseInt(cogs)
+      await user.ref.update({ coins: coins })
+      return res.send({ highestScore, cogs: coins })
+    } else {
+      await user.ref.update({ coins: Number.parseInt(cogs) })
+      return res.send({ highestScore, cogs: 0 })
+    }
   } catch (err) {
     console.log(err)
     return res.status(403).send({ message: err })
   }
 })
-
 
 module.exports = (app) => app.use('/user', router)
