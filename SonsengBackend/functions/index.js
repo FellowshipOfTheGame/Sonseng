dd3e550e0ff86e5adaf694ccf24d236afec96f04
danbarretto <daniel_barretto98@hotmail.com>
2020-09-26 20:47:05 -0300

    Add initUser firebase function

--- SonsengBackend/functions/index.js
+++ SonsengBackend/functions/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 const functions = require('firebase-functions')
 const bodyParser = require('body-parser')
+const admin = require('./controller/fb')
 
 app.use(cors({origin:true}))
 app.options('*', cors({origin:true}))
@@ -18,3 +19,18 @@ require('./controller/leaderboardController')(app)
 require('./controller/powerUpController')(app)
 require('./controller/userController')(app)
 exports.app = functions.https.onRequest(app)
+
+exports.initUser = functions.auth.user().onCreate((user)=>{
+    return admin.database().ref(`users/${user.uid}/`).update({
+        coins:0,
+        'highest-score':0,
+        'last-score':0,
+        'bought-powerUps':{
+            shield:{
+                level:0,
+                baseValue:10,
+                multiplier:1
+            }
+        }
+    })
+})
